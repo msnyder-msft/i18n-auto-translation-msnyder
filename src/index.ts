1e592f4824d2be2ed52ba339b5cb8fe6c24fecbf
Mitch Snyder <msnyder@microsoft.com>
2022-04-12 13:24:37 -0700

    Change back

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
 import { argv } from './translate/cli';
 import { TranslateSupplier } from './translate/translate-supplier';
 
-try {
-  await TranslateSupplier.getProvider(argv.apiProvider).translate();
-} catch (e) {
-  if (e instanceof Error) console.log(e.message);
-  else console.log(e);
-}
+TranslateSupplier.getProvider(argv.apiProvider)
+  .translate()
+  .catch((e) => {
+    if (e instanceof Error) console.log(e.message);
+    else console.log(e);
+  });
